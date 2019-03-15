63164fe919a257df5d74877b4666f3f125337f89
Michael Wonng <michael@wonng.com>
2019-03-16 00:51:56 +1100

    ready for main game app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Game from './Game'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Game</h1>
+        <Game />
       </div>
     );
   }
