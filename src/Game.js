464e791cb60dbf5aaec196e52636fae0f842ef25
Michael Wonng <michael@wonng.com>
2019-03-16 12:27:04 +1100

    traditional solution, but not best efficient

--- src/Game.js
+++ src/Game.js
@@ -13,13 +13,67 @@ class Game extends React.Component {
     }
   }
 
+  setActive = (arr,x,y) => {
+    arr[x][y] = Math.abs(1-arr[x][y]) ;
+    this.setState({
+      life: arr
+    })
+  }
+
+  goNextState = (arr) => {
+    let next = GameService.getNextState(arr)
+    this.setState({
+      life: next,
+    })
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value,
+    })
+  }
+
+  reset = () => {
+    const { row, column } = this.state;
+    let game = new GameService(parseInt(row,10), parseInt(column, 10));
+    let newArr = game.board;
+
+    this.setState({
+      life: newArr
+    })
+  }
 
   render () {
+    const { life } = this.state;
+    let rows = life.map((row, i)=>
+      <tr key={i}>
+        {row.map((col, j)=>
+          <td
+            key={j}
+            className={col > 0 ? "active" : ""}
+            onClick={()=> this.setActive(life, i, j)}
+            ></td>
+        )}
+      </tr>
+    )
     return (
       <div>
-        <h1>Game</h1>
+        <h1>Game of Life</h1>
         <table>
+          <tbody>
+            {rows}
+          </tbody>
         </table>
+        <div className="reset">
+          <label htmlFor="row">Rows:
+            <input type="number" name="row" value={this.state.row} onChange={(e)=>this.onChange(e)}/>
+          </label>
+          <label htmlFor="column">Column:
+            <input type="number" name="column" value={this.state.column} onChange={(e)=>this.onChange(e)} />
+          </label>
+          <button onClick={() => this.reset()}> Set & Reset </button>
+        </div>
+        <button onClick={() => this.goNextState(this.state.life)}> Next </button>
       </div>
     )
   }
--- src/func/game-service.js
+++ src/func/game-service.js
@@ -7,8 +7,49 @@ class GameService {
   }
 
   static getNextState(board) {
+    const maxRowIndex = board.length - 1;
+    const maxColunmIndex = board[0].length - 1;
 
+    const round = (i, j) => {
+      let count = 0;
+      if (i > 0 && board[i - 1][j]) count += 1;
+      if (i > 0 && j > 0 && board[i - 1][j - 1]) count += 1;
+      if (i > 0 && j < maxColunmIndex && board[i - 1][j + 1]) count += 1;
+
+      if (i < maxRowIndex && board[i + 1][j]) count += 1;
+      if (i < maxRowIndex && j > 0 && board[i + 1][j - 1]) count += 1;
+      if (i < maxRowIndex && j < maxColunmIndex && board[i + 1][j + 1]) count += 1;
+
+      if (j > 0 && board[i][j - 1]) count += 1;
+      if (j < maxColunmIndex && board[i][j + 1]) count += 1;
+
+      return count;
+    };
+
+    const tmp = {};
+
+    for (let i = 0; i <= maxRowIndex; i += 1) {
+      for (let j = 0; j <= maxColunmIndex; j += 1) {
+        const count = round(i, j);
+        if (board[i][j]) {
+          if (count !== 2 && count !== 3) {
+            if (!tmp[i]) tmp[i] = {};
+            tmp[i][j] = 0;
+          }
+        } else if (count === 3) {
+          if (!tmp[i]) tmp[i] = {};
+          tmp[i][j] = 1;
+        }
+      }
+    }
+
+    for (const row of Object.keys(tmp)) {
+      for (const col of Object.keys(tmp[row])) {
+        board[Number(row)][Number(col)] = tmp[row][col];
+      }
+    }
+      return board;
   }
 }
 
-export default GameService
+export default GameService;
