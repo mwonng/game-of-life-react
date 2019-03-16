777f3b15e69f13b8b457d8e8d131acec98cc184a
Michael Wonng <michael@wonng.com>
2019-03-17 03:35:16 +1100

    fix board larger than 10 index error

--- src/func/game-service.js
+++ src/func/game-service.js
@@ -16,12 +16,15 @@ class GameService {
   getNextState() {
     this.prepareChangeList();
     let activeList = this.getNextStateList();
+
     this.board = Array(this.row).fill().map(() => Array(this.col).fill(0));
+
     Object.keys(activeList).forEach(keyStr => {
-      let i = parseInt(keyStr[0], 10);
-      let j = parseInt(keyStr[1], 10);
+      let i = parseInt(keyStr.split('_')[0], 10);
+      let j = parseInt(keyStr.split('_')[1], 10);
       this.board[i][j] = 1;
-    })
+    });
+
     this.alive = activeList;
     this.dead = {};
     return this.board;
@@ -31,15 +34,16 @@ class GameService {
    * get all possible neighbours, including self, max 9 in array
    * return @array [ String, String ]
    */
-  getAroundKeys = (keyStr) => {
-    let rowNum = parseInt(keyStr[0], 10);
-    let colNum = parseInt(keyStr[1], 10);
-    let neighbours = []
+  getAroundKeys(keyStr) {
+    let rowNum = parseInt(keyStr.split('_')[0], 10);
+    let colNum = parseInt(keyStr.split('_')[1], 10);
+    let neighbours = [];
+
     for (let i=rowNum-1; i<=rowNum+1 ; i++) {
       if (i < 0 || i >= this.row) continue;
       for (let j=colNum-1; j<=colNum+1 ; j++) {
-        if (j < 0 || j >= this.row) continue;
-          neighbours.push(`${i}${j}`)
+        if (j < 0 || j >= this.col) continue;
+        neighbours.push(`${i}_${j}`);
       }
     }
     return neighbours;
@@ -52,10 +56,10 @@ class GameService {
    */
   switchCell(x,y) {
     this.board[x][y] = 1-this.board[x][y] ;
-    if (Object.keys(this.alive).includes(`${x}${y}`)) {
-      delete this.alive[`${x}${y}`]
+    if (Object.keys(this.alive).includes(`${x}_${y}`)) {
+      delete this.alive[`${x}_${y}`];
     } else {
-      this.alive[`${x}${y}`] = -1;
+      this.alive[`${x}_${y}`] = -1;
     }
   }
 
@@ -68,22 +72,25 @@ class GameService {
     this.board = Array(x).fill().map(() => Array(y).fill(0));
     this.row = x;
     this.col = y;
+    this.alive = {};
+    this.dead = {};
   }
 
   /**
    *
    * set new alive obj who might go dead or not
    * set new dead obj who might go alive or nor
-   * format: { '${rowIndex}${colIndex}': aliveNeighbourCount, ...}
+   * format: { '${rowIndex}_${colIndex}': aliveNeighbourCount, ...}
    *
    */
-  prepareChangeList = () =>{
-    let aliveKeys = Object.keys(this.alive)
-    aliveKeys.map( keyStr => {
+  prepareChangeList() {
+    let aliveKeys = Object.keys(this.alive);
+
+    aliveKeys.forEach( keyStr => {
       let neighbours = this.getAroundKeys(keyStr);
       neighbours.forEach( k => {
-        let row = parseInt(k[0], 10);
-        let col = parseInt(k[1], 10);
+        let row = parseInt(k.split('_')[0], 10);
+        let col = parseInt(k.split('_')[1], 10);
 
         if (this.board[row][col] < 1) {
           if (Object.keys(this.dead).includes(k)) {
@@ -94,26 +101,25 @@ class GameService {
         } else {
           this.alive[k] += 1;
         }
-
       })
     })
   }
 
   /**
    * after prepareChangeList(), create a new array for next generation with two obj in prepareChangeList()
-   * @return: [ '${rowIndex}${colIndex}', ]
+   * @return: [ '${rowIndex}_${colIndex}', ]
    */
-  getNextStateList = () => {
+  getNextStateList() {
     let result = {};
 
     // still alive
-    let nextAlive = Object.keys(this.alive).filter( k => this.alive[k] === 2 || this.alive[k] ===3 )
+    let nextAlive = Object.keys(this.alive).filter( k => this.alive[k] === 2 || this.alive[k] === 3 );
 
     // dead to alive
     let deadToAlive = Object.keys(this.dead).filter( k => this.dead[k] === 3 )
 
     let nextActive = [...nextAlive, ...deadToAlive];
-    nextActive.forEach(key => result[key]=-1);
+    nextActive.forEach(key => result[key] = -1);
     return result;
   }
 }
--- test/Game/game-service.test.js
+++ test/Game/game-service.test.js
@@ -12,5 +12,64 @@ test('initService', t => {
 
 
 test('getNextState()', t => {
-  t.pass();
+  let game = new GameService(6,6);
+  game.switchCell(1,2);
+  game.switchCell(2,3);
+  game.switchCell(3,1);
+  game.switchCell(3,2);
+  game.switchCell(3,3);
+  game.getNextState();
+  t.deepEqual(game.board, [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,1,0,1,0,0],
+    [0,0,1,1,0,0],
+    [0,0,1,0,0,0],
+    [0,0,0,0,0,0],
+  ]);
+  game.getNextState();
+  t.deepEqual(game.board, [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,1,0,0],
+    [0,1,0,1,0,0],
+    [0,0,1,1,0,0],
+    [0,0,0,0,0,0],
+  ]);
+  game.getNextState();
+  t.deepEqual(game.board, [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,1,0,0,0],
+    [0,0,0,1,1,0],
+    [0,0,1,1,0,0],
+    [0,0,0,0,0,0],
+  ]);
+  game.getNextState();
+  t.deepEqual(game.board, [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,1,0,0],
+    [0,0,0,0,1,0],
+    [0,0,1,1,1,0],
+    [0,0,0,0,0,0],
+  ]);
+});
+
+test('switchCell()', t => {
+  let game = new GameService(6,4);
+  game.switchCell(1,3);
+  t.is(game.board[1][3], 1,  "enable cell");
+  game.switchCell(1,3);
+  t.is(game.board[1][3], 0,  "disenable cell");
+});
+
+test('getAroundKeys()', t => {
+  let game = new GameService(6,4);
+  let keys = game.getAroundKeys('00');
+  t.deepEqual(keys, ['00','01','10','11'], "getAroundKeys in coener is wrong");
+  let keysInMid = game.getAroundKeys('12');
+  t.deepEqual(keysInMid, ['01','02','03','11','12','13','21','22','23'], "getAroundKeys in middle is wrong");
+  let keysInSide = game.getAroundKeys('33');
+  t.deepEqual(keysInSide, ['22','23','32','33','42','43'], "getAroundKeys in sidebar is wrong");
 });
